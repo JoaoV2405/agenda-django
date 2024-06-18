68f3a6d6a1931bd1183924a0fde039a531f759b3
Joao <mendoncajoao2323@gmail.com>
2024-06-18 12:28:08 -0300

    adicionada tabelas de evento

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from core.models import Evento
+
 # Register your models here.
+class Evento_Admin(admin.ModelAdmin):
+    #faz essas opçoes aparecerem na tabela
+    list_display = ('titulo', 'data_criacao', 'data_evento')
+    list_filter = ('titulo', )
+
+
+
+admin.site.register(Evento, Evento_Admin)
--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null= True)
+    data_evento = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now=True)
+    
+    # importa a classe User do proprio django, on_delete=models.CASCADE
+    # é o comportamento do django caso o user seja deletado, cascade indica
+    # que todos os eventos do user deve ser deletados
+    
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    class Meta: 
+        db_table = 'evento'
+        
+    def __str__(self):
+        return self.titulo
